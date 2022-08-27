e8789c8deb91a7e3fea16ce49119e6a26eed807e
Jong Min <jmbaek07@gmail.com>
2022-08-27 12:04:58 +0900

    refactoring: 코드 추가

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
-const logger = require('./config/logger');
-const express = require('./config/express');
-const app = express();
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config({ path: path.join(__dirname + "/../../.env") });
 
-app.listen(5000, ()=>{
-    try{
-        logger.info(`[${process.env.NODE_ENV}] API Server Start At Port ${process.env.PORT}`);
-    } catch(err){
-        console.log(err);
+const https = require("https");
+const http = require("http");
+
+const logger = require("./src/middleware/package/logg/logger");
+
+const fs = require("fs");
+const app = require("./src/middleware/express");
+
+const HTTPS_PORT = process.env.HTTPS_PORT;
+
+console.log(process.env.NODE_ENV);
+
+const options = {
+    key: fs
+        .readFileSync
+        // 작성하세용
+        (),
+    cert: fs
+        .readFileSync
+        // 작성하세용
+        (),
+};
+
+// Create an HTTPS server.
+// https.createServer(options, app).listen(HTTPS_PORT, () => {
+//     try {
+//         logger.info(
+//             `[${process.env.NODE_ENV}] API Server Start At Port ${HTTPS_PORT}`
+//         );
+//     } catch (error) {
+//         logger.error(error);
+//     }
+// });
+
+http.createServer(app).listen(HTTPS_PORT, () => {
+    try {
+        logger.info(
+            `[${process.env.NODE_ENV}] API Server Start At Port ${HTTPS_PORT}`
+        );
+    } catch (error) {
+        logger.error(error);
     }
-})
+});
