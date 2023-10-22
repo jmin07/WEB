898489413d2674dce77576e0e22ee261ffa7b145
Baek Jong Min <jongmin.baek@ssnc.co.kr>
2023-10-22 11:39:22 +0900

    [오류 처리] 에러 페이지 및 오류 처리

--- src/main/java/farmstory/myfarm/user/controller/LoginController.java
+++ src/main/java/farmstory/myfarm/user/controller/LoginController.java
@@ -1,8 +1,10 @@
 package farmstory.myfarm.user.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class LoginController {
 
